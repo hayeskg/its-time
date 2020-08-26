edbc201f854c38ccb7d7d8d517dda2db74b82b3f
hayeskg <kristof.g.hayes@gmail.com>
2020-08-26 10:18:33 +0200

    Sofia Pro fonts added, Input form styled

--- components/form.js
+++ components/form.js
@@ -1,5 +1,36 @@
 import styles from './form.module.css'
-import { TextField, Button } from '@material-ui/core';
+// import { TextField, Button } from '@material-ui/core';
+import styled from "styled-components";
+
+
+const StyledButton = styled.button`
+  background-color: #F73A54;
+  font-family: 'Sofia Pro';
+  font-size: 22px;
+  font-weight: 700;
+  color: #FFFFFF;
+  height: 60px;
+  width: 153px;
+  border-top-style: hidden;
+  border-right-style: hidden;
+  border-left-style: hidden;
+  border-bottom-style: hidden;
+`;
+
+const StyledInput = styled.input`
+  background-color: #F2F2F2;
+  font-family: 'Sofia Pro';
+  font-size: 22px;
+  font-weight: 400;
+  color: #222222;
+  height: 60px;
+  width: 401px;
+  border-top-style: hidden;
+  border-right-style: hidden;
+  border-left-style: hidden;
+  border-bottom-style: hidden;
+  
+`;
 
 export default function Form() {
   return (
@@ -9,11 +40,10 @@ export default function Form() {
     </h1>
       <p>We’ll send you updates and news on the It’s Time event</p>
       <form>
-        <TextField id="outlined-basic" label="Enter your email address" variant="outlined" />
-        <Button variant="contained" color="secondary">
-          Submit
-</Button>
+        <StyledInput type="text" required />
+        <StyledButton> Submit </StyledButton>
       </form>
     </div>
   )
 }
+
--- pages/index.js
+++ pages/index.js
@@ -10,25 +10,25 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <div className={styles.title}>
-          <img src="/images/asset13.png" alt="it's time graphic wide" />
-        </div>
+      {/* <main className={styles.main}> */}
+      <div className={styles.title}>
+        <img src="/images/asset13.png" alt="it's time graphic wide" />
         <div className={styles.subtitle}>
           <p>Coming November 2020</p>
         </div>
-        <div className={styles.para1}>
-          <p>On 1 November, a week-long United Nations <br />Climate Change Conference was supposed to <br />take place in Glasgow Scotland. Due to the <br />Coronavirus pandemic, it’s been postponed.</p>
-          <br />
-          <h1>That's not good enough.</h1>
-        </div>
-        <div className={styles.para2}>
-          <p>While COVID-19 has occupied our attention in 2020, the <br /> world has continued to burn and this year is set to be the <br />hottest on record. It’s time to take matters into our own <br />hands. It’s time for a conference focused on real change. It’s <br />time for a global festival of direct action.</p>
-          <br />
-          <h1>It's time to do something.</h1>
-        </div>
-        <Form className={styles.form} />
-      </main>
+      </div>
+      <div className={styles.para1}>
+        <p>On 1 November, a week-long United Nations <br />Climate Change Conference was supposed to <br />take place in Glasgow Scotland. Due to the <br />Coronavirus pandemic, it’s been postponed.</p>
+        <br />
+        <h1>That's not good enough.</h1>
+      </div>
+      <div className={styles.para2}>
+        <p>While COVID-19 has occupied our attention in 2020, the <br /> world has continued to burn and this year is set to be the <br />hottest on record. It’s time to take matters into our own <br />hands. It’s time for a conference focused on real change. It’s <br />time for a global festival of direct action.</p>
+        <br />
+        <h1>It's time to do something.</h1>
+      </div>
+      <Form className={styles.form} />
+      {/* </main> */}
 
       <footer className={styles.footer}>
         <h3>Footer TBC</h3>
